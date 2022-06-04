5afd4f3a2d595a6dd8e320492b556527a13d6e3b
GuilhermeRibeiro0 <guilhermeribeir0@hotmail.com>
2022-06-04 18:31:57 -0300

    endereco component

--- src/app/endereco/endereco.component.ts
+++ src/app/endereco/endereco.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Endereco } from '../models/endereco.model';
+import { EnderecoService } from '../services/endereco.service';
 
 @Component({
   selector: 'app-endereco',
@@ -7,9 +9,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EnderecoComponent implements OnInit {
 
-  constructor() { }
+  lista: Endereco[] = [];
+  obj: Endereco = new Endereco();
+  mens =  '';
+
+  constructor(private api:EnderecoService) { }
 
   ngOnInit(): void {
+    this.consultar();
+  }
+  consultar() {
+    this.api.consultar()
+    .toPromise()
+    .then
+    (res => {
+      this.lista = res!;
+    }); 
+  }
+  adicionar(){
+    this.api.adicionar(this.obj)
+    .toPromise()
+    .then(endereco =>{
+      this.mens = "Endereço Adicionado com sucesso";
+      this.consultar();
+    });
+  }
+  excluir(){
+    this.api.excluir(this.obj.id)
+    .toPromise()
+    .then(endereco =>{
+      this.mens = "Endereço excluido";
+      this.consultar();
+    });
+  }
+  alterar(){
+    this.api.alterar(this.obj.id,this.obj)
+    .toPromise()
+    .then(endereco =>{
+      this.mens = "Endereço Alterado com sucesso"
+      this.consultar();
+    });
+  }
+  carregarDados(e:Endereco){
+    this.obj = e;
   }
 
 }
